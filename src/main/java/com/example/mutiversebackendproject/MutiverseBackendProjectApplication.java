a6d928175be7a2715b80ec86b3c6a67b6c150154
yyogue <youssouf.e.yogue@aexp.com>
2023-01-22 15:46:58 -0600

    First commit

---
+++ src/main/java/com/example/mutiversebackendproject/MutiverseBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mutiversebackendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutiverseBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutiverseBackendProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mutiversebackendproject/MutiverseBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mutiversebackendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutiverseBackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
